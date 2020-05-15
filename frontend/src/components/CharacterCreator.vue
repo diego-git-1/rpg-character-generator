30298a7c545c44700cc35f1c76b777caeaef3123
diego-git-1 <diegocesar.dev@gmail.com>
2020-05-15 11:46:27 -0300

    feat: end frontend

---
+++ frontend/src/components/CharacterCreator.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="character-Creator">
+    <h1>Character Creator</h1>
+    <label for="character-name">Character Name: </label>
+    <input type="text" id="character-name" v-model="name" placeholder="Enter a name" /> <br /><br />
+    <label for="professions-list">Character Profession: </label>
+    <select id="professions-list" v-model="profession">
+      <option value="Mage">Mage</option>
+      <option value="Thief">Thief</option>
+      <option value="Warrior">Warrior</option>
+    </select>
+    <p>{{ name }}</p>
+    <p>{{ profession }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CharacterCreator',
+    data: function () {
+      return {
+        name: "",
+        profession: ""
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ frontend/src/components/CharacterViewer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="character-viewer">
+    <h1>Character Viewer</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CharacterViewer',
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
