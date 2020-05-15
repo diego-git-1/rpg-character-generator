af00959e6fd15fa68511d0c63609202b9d66e6fe
diego-git-1 <diegocesar.dev@gmail.com>
2020-05-15 13:59:55 -0300

    feat: add create controller

--- backend/src/controllers/CharacterController.js
+++ backend/src/controllers/CharacterController.js
@@ -4,5 +4,14 @@ module.exports = {
   async index(req, res) {
     const characters = await Character.find()
     res.json(characters)
+  },
+
+  async create(req, res) {
+    const { name, profession } = req.body
+    const character = await Character.create({
+      name,
+      profession
+    })
+    res.json(character)
   }
 }
