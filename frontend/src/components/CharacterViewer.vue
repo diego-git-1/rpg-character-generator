0a003d00a2f692f74776e7b079231c471098267a
diego-git-1 <diegocesar.dev@gmail.com>
2020-05-15 13:14:05 -0300

    feat: add axios

--- frontend/src/components/CharacterViewer.vue
+++ frontend/src/components/CharacterViewer.vue
@@ -1,12 +1,30 @@
 <template>
   <div class="character-viewer">
     <h1>Character Viewer</h1>
+    <p>{{ characters }}</p>
   </div>
 </template>
 
 <script>
+  import axios from 'axios'
+
   export default {
     name: 'CharacterViewer',
+    data: function () {
+      return {
+        characters: null
+      }
+    },
+    methods: {
+      getCharacters: function () {
+        axios
+          .get('http://localhost:3000/characters')
+          .then(response => (this.characters = response.data))
+      }
+    },
+    mounted: function () {
+      this.getCharacters()
+    }
   }
 </script>
 
