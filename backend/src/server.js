5eb7f0538a51340bfed266345c06c32da3fad69c
diego-git-1 <diegocesar.dev@gmail.com>
2020-05-15 13:58:59 -0300

    fix: add characters to the routes middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const cors = require('cors')
 const routes = require('./routes')
+const cors = require('cors')
 require('dotenv').config()
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -15,6 +15,6 @@ const PORT = 3000
 
 app.use(express.json())
 app.use(cors())
-app.use(routes)
+app.use('/characters', routes)
 
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}/`))
